8535e918e778357b157ff30676dbcd502a5d3529
Çağdaş Olam <44732969+cagdasolam@users.noreply.github.com>
2023-10-29 18:18:35 +0300

    added quastionnaire page

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,27 @@
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <a href="/" className="nav-title">Questionnare</a>
+      <div className="menu">
+        <ul className="nav-ul">
+          <li className="nav-item">
+            <NavLink to="/home" className="nav-link">Home</NavLink>
+          </li>
+          <li className="nav-item" >
+            <NavLink to="/about" className="nav-link">About</NavLink>
+          </li>
+          <li className="nav-item" >
+            <NavLink to="/sign-in" className="nav-link">Log in</NavLink>
+          </li>
+          <li className="nav-item" >
+            <NavLink to="/create-account" className="create-account">Create Account</NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
